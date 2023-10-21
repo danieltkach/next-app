b96206e93ccae04f23a136d1374458d4fbb3ffb1
Daniel Tkach <danieltkach@gmail.com>
2023-10-20 21:16:19 -0300

    exploring nextjs

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react';
+
+const AddToCart =()=> {
+  return(
+    <button onClick={()=>console.log('Click')}>Add to Click</button>
+  )
+}
+
+export default AddToCart;
